8cf361c12314ac6e48d0f14ac7040602f8c07414
Heather Zurek <heather.zurek@gmail.com>
2019-03-29 10:11:44 -0700

    add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     var location = $("#location").val();
     var type = $("#type").val();
-    // var interest = $("#interest").val();
-    // var company = $("#company").val();
-    // var android = $("#android").val();
+    var interest = $("#interest").val();
+    var company = $("#company").val();
+    var android = $("#android").val();
 
     if (type === "1") {
-          $(".typeOne").show();
-    } else {
-
+      $(".typeTwo").hide();
+      $(".typeOne").show();
+    } else if ( type === "2" && company === "2" && interest === "2" ) {
+      $(".typeTwo").show();
     }
+
 
     event.preventDefault();
   });
